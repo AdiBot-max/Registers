e9d719c72920dd1da26ba6abe5142a8b983f54ac
AdiBot-max <adichauhan12354@gmail.com>
2026-04-26 23:28:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // 🔥 REPLACE THESE
-const supabaseUrl = "https://YOUR_PROJECT.supabase.co";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseUrl = "https://lfnlgxeavfxpngdlaipn.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmbmxneGVhdmZ4cG5nZGxhaXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjAzODUsImV4cCI6MjA5Mjc5NjM4NX0.c1NI2KnjnYVqsITU3nXbFg4ydOJk5-b2WYR2ZcpWLLg";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
